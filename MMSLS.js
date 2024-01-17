45ce6634c1090c7d49f50f4c71a7576027493d5e
ninkai <38028859+ninkai@users.noreply.github.com>
2024-01-18 03:59:38 +0800

    Update MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -20,7 +20,7 @@ const MMSLS = [
  */
 const MMSLS = [
     
-    "https://www.maimemo.com/page?sid=1e7735918bba16a08b899b6ee9b3732e&uid=33471468&pid=662f5e00a288c231a644e71024835ef0&tid=547ee91c9fa8ec744c6ca375a0e21748&view_time=1705433160&bind_user=0&create_user=0&oauth=wechat_wx6a4680fa1ef1b496&unionid=o6IDit47XZp6XIJnjNFc6sTTwTo4",
+    "https://www.maimemo.com/page?sid=0528d167f4a5bdba09f6dfe5585dcc18&uid=33471468&pid=fd7d3e7267b739fe7845d45676917685&tid=230bba63fb97fbe9500e73211e78a27a&view_time=1705521395&bind_user=0&create_user=0&oauth=wechat_wx6a4680fa1ef1b496&unionid=o6IDit47XZp6XIJnjNFc6sTTwTo4",
 
 
   ]
